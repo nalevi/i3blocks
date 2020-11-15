52f3a273d5b9a22e22e76c68e82c31eb846040e9
nalevi17 <woundless17@gmail.com>
2020-11-15 17:48:50 +0100

    battery ikon kusztomizálás

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -6,10 +6,10 @@ CHR=$(acpi -b | grep -E -o '(Charging|Discharging)')
 [[ $CHR == "Discharging" ]] && CHR=''
 
 
-[ ${BAT%?} -ge 85 ] && echo "$BAT  $CHR"
-[ ${BAT%?} -le 84 ] && echo "$BAT  $CHR"
-[ ${BAT%?} -le 70 ] && echo "$BAT  $CHR"
-[ ${BAT%?} -le 50 ] && echo "$BAT  $CHR"
-[ ${BAT%?} -le 20 ] && echo "$BAT  $CHR"
-[ ${BAT%?} -le 5 ] && echo "#f12711"
+[ ${BAT%?} -ge 76 ] && echo "$BAT  $CHR"
+[ ${BAT%?} -le 75 ] && echo "$BAT  $CHR"
+[ ${BAT%?} -le 50 ] && echo "$BAT  $CHR"
+[ ${BAT%?} -le 25 ] && echo "$BAT  $CHR"
+#[ ${BAT%?} -le 20 ] && echo "$BAT  $CHR"
+[ ${BAT%?} -le 5 ] && echo "#f12711" && echo "$BAT  $CHR"
 
