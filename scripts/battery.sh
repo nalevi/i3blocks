b8efefc74482f7032cbf09b36221be442d1e6d19
nalevi <woundless17@gmail.com>
2020-11-21 19:50:45 +0100

    Volume label

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -2,14 +2,16 @@
 
 BAT=$(acpi -b | grep -E -o '[0-9][0-9]?%')
 CHR=$(acpi -b | grep -E -o '(Charging|Discharging)')
+RED='\033[0;31m'
+NC='\033[0m' # No Color
 
 [[ $CHR == "Discharging" ]] && CHR=''
 
 
 [ ${BAT%?} -ge 76 ] && echo "$BAT  $CHR"
-[ ${BAT%?} -le 75 ] && echo "$BAT  $CHR"
-[ ${BAT%?} -le 50 ] && echo "$BAT  $CHR"
+#[ ${BAT%?} -le 5 ] && echo -e "$BAT ${RED} ${NC} $CHR"
+[ ${BAT%?} -le 10 ] && echo "$BAT  $CHR"
 [ ${BAT%?} -le 25 ] && echo "$BAT  $CHR"
-#[ ${BAT%?} -le 20 ] && echo "$BAT  $CHR"
-[ ${BAT%?} -le 5 ] && echo "#f12711" && echo "$BAT  $CHR"
+[ ${BAT%?} -le 50 ] && echo "$BAT  $CHR"
+[ ${BAT%?} -le 75 ] && echo "$BAT  $CHR"
 
