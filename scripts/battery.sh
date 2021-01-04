ef717e5d1ce0b54a0565a1003078363c0581dcf0
nalevi <woundless17@gmail.com>
2021-01-04 17:46:00 +0100

    battery stuff

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -8,10 +8,11 @@ NC='\033[0m' # No Color
 [[ $CHR == "Discharging" ]] && CHR=''
 
 
-[ ${BAT%?} -ge 76 ] && echo "$BAT  $CHR"
+[ ${BAT%?} -ge 90 ] && echo "$BAT  $CHR"
 #[ ${BAT%?} -le 25 ] && echo "#FF8000"
 [ ${BAT%?} -le 10 ] && echo "$BAT  $CHR"
 [ ${BAT%?} -le 25 ] && echo "$BAT  $CHR"
 [ ${BAT%?} -le 50 ] && echo "$BAT  $CHR"
 [ ${BAT%?} -le 75 ] && echo "$BAT  $CHR"
+[ ${BAT%?} -le 89 ] && echo "$BAT  $CHR"
 
