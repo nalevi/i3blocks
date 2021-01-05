d4a2f5dbfaa188cb945304f888b873cea28fc951
nalevi <woundless17@gmail.com>
2021-01-05 16:18:27 +0100

    charging proba

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -7,9 +7,14 @@ NC='\033[0m' # No Color
 
 [[ $CHR == "Discharging" ]] && CHR=''
 
+[[ ${BAT%?} -ge 90 && ${CHR} == "Charging" ]] && echo "$BAT  "
+[ ${BAT%?} -le 10 ] && [ ${CHR} = "Charging" ] && echo "$BAT  "
+[ ${BAT%?} -le 25 ] && [ ${CHR} = "Charging" ] && echo "$BAT  "
+[ ${BAT%?} -le 50 ] && [ ${CHR} = "Charging" ] && echo "$BAT  "
+[ ${BAT%?} -le 75 ] && [ ${CHR} = "Charging" ] && echo "$BAT  "
+[[ ${BAT%?} -le 89 && $CHAR == "Charging" ]] && echo "$BAT  "
 
 [ ${BAT%?} -ge 90 ] && echo "$BAT  $CHR"
-#[ ${BAT%?} -le 25 ] && echo "#FF8000"
 [ ${BAT%?} -le 10 ] && echo "$BAT  $CHR"
 [ ${BAT%?} -le 25 ] && echo "$BAT  $CHR"
 [ ${BAT%?} -le 50 ] && echo "$BAT  $CHR"
