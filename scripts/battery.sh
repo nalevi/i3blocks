37bb6e1dfcb38ef7a08fbc4f119efc6a6cf5feb7
nalevi <woundless17@gmail.com>
2021-01-05 16:26:03 +0100

    juhi

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -5,19 +5,17 @@ CHR=$(acpi -b | grep -E -o '(Charging|Discharging)')
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-[[ $CHR == "Discharging" ]] && CHR=''
-
 [[ ${BAT%?} -ge 90 && ${CHR} == "Charging" ]] && echo "$BAT  "
-[ ${BAT%?} -le 10 ] && [ ${CHR} = "Charging" ] && echo "$BAT  "
-[ ${BAT%?} -le 25 ] && [ ${CHR} = "Charging" ] && echo "$BAT  "
-[ ${BAT%?} -le 50 ] && [ ${CHR} = "Charging" ] && echo "$BAT  "
-[ ${BAT%?} -le 75 ] && [ ${CHR} = "Charging" ] && echo "$BAT  "
-[[ ${BAT%?} -le 89 && $CHAR == "Charging" ]] && echo "$BAT  "
+[[ ${BAT%?} -le 10 && ${CHR} == "Charging" ]] && echo "$BAT  "
+[[ ${BAT%?} -le 25 && ${CHR} == "Charging" ]] && echo "$BAT  "
+[[ ${BAT%?} -le 50 && ${CHR} == "Charging" ]] && echo "$BAT  "
+[[ ${BAT%?} -le 75 && ${CHR} == "Charging" ]] && echo "$BAT  "
+[[ ${BAT%?} -le 89 && $CHR == "Charging" ]] && echo "$BAT  "
 
-[ ${BAT%?} -ge 90 ] && echo "$BAT  $CHR"
-[ ${BAT%?} -le 10 ] && echo "$BAT  $CHR"
-[ ${BAT%?} -le 25 ] && echo "$BAT  $CHR"
-[ ${BAT%?} -le 50 ] && echo "$BAT  $CHR"
-[ ${BAT%?} -le 75 ] && echo "$BAT  $CHR"
-[ ${BAT%?} -le 89 ] && echo "$BAT  $CHR"
+[ ${BAT%?} -ge 90 ] && echo "$BAT  "
+[ ${BAT%?} -le 10 ] && echo "$BAT  "
+[ ${BAT%?} -le 25 ] && echo "$BAT  "
+[ ${BAT%?} -le 50 ] && echo "$BAT  "
+[ ${BAT%?} -le 75 ] && echo "$BAT  "
+[ ${BAT%?} -le 89 ] && echo "$BAT  "
 
